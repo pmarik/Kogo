4861ae6346f5e74520470e8648f9195dd4189444
pmarik <pmarik@uwalumni.com>
2019-11-25 15:23:24 -0600

    intersection observer added to home to control second stem animation

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,39 @@
 "use strict";
+require('intersection-observer');
 
 function init(){
+
+    // window.setTimeout(() => {
+    //     document.getElementById('vine2').classList.add('theOverlay2');
+    //     document.getElementsByClassName('svgContain')[1].setAttribute("style", "visibility: visible;");
+    // }, 2000);
+
+
+    let thresholdNum = 0.8
+    const options = {
+        root: null,
+        rootMargin: '0px',
+        threshold: thresholdNum
+    }
+
+    let stem2 = document.getElementById('stem2');
+    
+
+    let callback = function(entries, observer) {
+        entries.forEach(entry => {
+          if(entry.intersectionRatio > thresholdNum){
+            document.getElementById('vine2').classList.add('theOverlay2');
+            document.getElementsByClassName('svgContain')[1].setAttribute("style", "visibility: visible;");
+
+            observer.unobserve(stem2);
+          }
+        }); 
+      }
+      
    
+
+    let observer = new IntersectionObserver(callback, options);
+    observer.observe(stem2);
 }
 
 document.addEventListener("DOMContentLoaded", function(){
@@ -11,7 +43,13 @@ document.addEventListener("DOMContentLoaded", function(){
         let heroTarget = document.getElementsByClassName('hero-anim-target');
         heroTarget[0].classList.add('hero-anim');
         heroTarget[1].classList.add('hero-info-anim');
-    }, 200)
+
+        //Add class to animate SVG, initial start state is hidden until content loaded
+        document.getElementById('vine1').classList.add('theOverlay1');
+        document.getElementsByClassName('svgContain')[0].setAttribute("style", "visibility: visible;");
+
+
+    }, 200);
     
     init();
   });
